726c2b9c30854bd72dd1944c9ca85405a63bf1f5
Ifeanyi Ikesee <anyiones@yahoo.ca>
2022-10-24 09:39:56 +0300

    First task

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -3,7 +3,7 @@ package com.mjc.stage0;
 public class FirstApplication {
     //string greetings = "Hello, World";
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
 
     }
 }
